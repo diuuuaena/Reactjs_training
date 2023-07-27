dc8baad17a66da56449e219163878dd0069a4361
Phong <phongnguyen22588@gmail.com>
2023-07-28 02:29:55 +0700

    UI Header#3

---
+++ tiktok/src/components/Button/index.js
@@ -0,0 +1,65 @@
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    disabled = false,
+    rounded = false,
+    small = false,
+    large = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...Passprops
+}) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...Passprops,
+    };
+
+    // Remove event listener when btn is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        text,
+        disabled,
+        rounded,
+        small,
+        large,
+    });
+    return (
+        <Comp className={classes} {...props}>
+            <span>{children}</span>
+        </Comp>
+    );
+}
+
+export default Button;
